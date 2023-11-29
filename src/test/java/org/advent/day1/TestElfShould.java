0f53feadd5047ac56c35f00f7b4a389c951c561a
keneo <kene_obi@yahoo.com>
2023-11-29 10:12:50 +0000

    Solution

--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -16,21 +16,39 @@ public class TestElfShould {
 
     @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+        Elf elf = new Elf();
+        elf.addCalories(1000);
+
+        assertThat(elf.getTotalCalories(), equalTo(1000));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        Elf firstElf = new Elf();
+        Elf secondElf = new Elf();
+        firstElf.addCalories(2500);
+        secondElf.addCalories(1000);
+
+        assertThat(firstElf.compareTo(secondElf), greaterThan(0));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        Elf firstElf = new Elf();
+        Elf secondElf = new Elf();
+        firstElf.addCalories(2500);
+        secondElf.addCalories(2500);
+
+        assertThat(firstElf.compareTo(secondElf), equalTo(0));
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        Elf firstElf = new Elf();
+        Elf secondElf = new Elf();
+        firstElf.addCalories(1000);
+        secondElf.addCalories(2500);
+
+        assertThat(firstElf.compareTo(secondElf), lessThan(0));
     }
 }
